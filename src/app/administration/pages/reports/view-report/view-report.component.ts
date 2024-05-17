4ccf4e2df78e2df6299adf3b4f5e3319fd25412e
Alewoman <yemimaaleman@gmail.com>
2024-05-17 16:14:51 +0300

    reports

--- src/app/administration/pages/reports/view-report/view-report.component.ts
+++ src/app/administration/pages/reports/view-report/view-report.component.ts
@@ -9,6 +9,7 @@ import { Subscription } from 'rxjs';
 import { DataStoreService } from 'src/@core/helpers/data-store.service';
 import { ReportsService } from 'src/app/administration/Service/reports/reports.service';
 import { DynamicReportLookupComponent } from '../dynamic-report-lookup/dynamic-report-lookup.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface ReportInterface {
   name: string;
@@ -56,155 +57,180 @@ export class ViewReportComponent implements OnInit {
   groupAccountsreport: boolean = true;
   ChartsOfAccounts: any;
 
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
+  pageIndex: number = 0;
+  pageSize: number = 20;
+  incomedisplayedColumns: string[] = ['description', 'notes', '2024-amount(Ksh)','2023-amount(Ksh)'];
+  balancedisplayedColumns: string[] = ['description', '2024-amount(Ksh)','2023-amount(Ksh)']
+
   constructor(
     private dialog: MatDialog,
     private _snackBar: MatSnackBar,
     private reportsService: ReportsService,
     private dataStoreApi: DataStoreService
   ) {
-    this.iniAuthorization();
+    // this.iniAuthorization();
   }
   ngOnInit(): void {}
 
-  iniAuthorization() {
-    console.log('Init auth');
-    this.actionsArray = this.dataStoreApi.getActions('REPORTS');
-    for (let i = 0; i < this.actionsArray.length; i++) {
-      let obj = this.actionsArray[i];
-      if (!this.retailcustomerreport)
-        this.retailcustomerreport = obj.name == 'RETAIL CUSTOMER REPORT';
-     }
 
-    this.initDisplay();
-    this.sortAuthReports();
-    this.uiReports = this.authReportDisplay;
+  generateIncomeStatement(){
+   //awaiting API logics
   }
 
-  sortAuthReports() {
-    var tempDisplay: ReportCategory[] = [];
-    this.reportDisplay.forEach((element) => {
-      if (!element.display) {
-      } else {
-        var newElement: ReportCategory = JSON.parse(JSON.stringify(element));
-        newElement.specificReports = [];
-        element.specificReports.forEach((report) => {
-          if (report.display) {
-            newElement.specificReports.push(report);
-          }
-        });
-        if (newElement.specificReports.length > 0) {
-          tempDisplay.push(newElement);
-        }
-      }
-    });
-    this.authReportDisplay = tempDisplay;
+  generateBalanceSheet(){
+ //awaiting API logics
   }
 
-  onSearchChange() {
-    this.search();
+  generateStatementofequity(){
+   //awaiting API logics
   }
 
-  search() {
-    var tempDisplay: ReportCategory[] = [];
-    this.authReportDisplay.forEach((element) => {
-      if (
-        element.title.toUpperCase().indexOf(this.searchTerm.toUpperCase()) !==
-        -1
-      ) {
-        tempDisplay.push(element);
-      } else {
-        var newElement: ReportCategory = JSON.parse(JSON.stringify(element));
-        newElement.specificReports = [];
-        element.specificReports.forEach((report) => {
-          if (
-            report.name.toUpperCase().indexOf(this.searchTerm.toUpperCase()) !==
-            -1
-          ) {
-            newElement.specificReports.push(report);
-          }
-        });
-        if (newElement.specificReports.length > 0) {
-          tempDisplay.push(newElement);
-        }
-      }
-    });
-    this.uiReports = tempDisplay;
+  generateCashflowStatement(){
+    //awaiting API logics
   }
 
-  toDynamicReport = (report: ReportInterface): void => {
-    console.log('To dynamic report');
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = false;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = '800px';
-    dialogConfig.data = {
-      report: report,
-      reportTitle: report.name,
-    };
-    const dialogRef = this.dialog.open(
-      DynamicReportLookupComponent,
-      dialogConfig
-    );
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(result);
-      if (result != null && result.event == 'display') {
-        this.displayReport = true;
-        this.displayContents = result;
-      }
-    });
-  };
-
-  initDisplay = (): void => {
-    console.log(this.retailcustomerreport);
-    this.reportDisplay = [
-      {
-        title: 'Ticket Reports',
-        display: true,
-        specificReports: [
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'all_members.jrxml',
-            name: 'Customer Report',
-            display: this.retailcustomerreport,
-          },
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'real_members.jrxml',
-            name: 'Category Report',
-            display: this.retailcustomerreport,
-          },
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'member_by_category.jrxml',
-            name: 'Ticket Assignee Report',
-            display: this.retailcustomerreport,
-          },
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'all_members.jrxml',
-            name: 'All Ticket REPORT',
-            display: this.retailcustomerreport,
-          },
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'all_members.jrxml',
-            name: 'Ticket Status REPORT',
-            display: this.retailcustomerreport,
-          },
-          {
-            isDynamic: true,
-            requiredFields: ['fromdate', 'todate'],
-            filename: 'all_members.jrxml',
-            name: 'Institution Ticket REPORT',
-            display: this.retailcustomerreport,
-          },
-        ],
-      }, 
-    ];
-  };
 }
+
+//   iniAuthorization() {
+//     console.log('Init auth');
+//     this.actionsArray = this.dataStoreApi.getActions('REPORTS');
+//     for (let i = 0; i < this.actionsArray.length; i++) {
+//       let obj = this.actionsArray[i];
+//       if (!this.retailcustomerreport)
+//         this.retailcustomerreport = obj.name == 'RETAIL CUSTOMER REPORT';
+//      }
+
+//     this.initDisplay();
+//     this.sortAuthReports();
+//     this.uiReports = this.authReportDisplay;
+//   }
+
+//   sortAuthReports() {
+//     var tempDisplay: ReportCategory[] = [];
+//     this.reportDisplay.forEach((element) => {
+//       if (!element.display) {
+//       } else {
+//         var newElement: ReportCategory = JSON.parse(JSON.stringify(element));
+//         newElement.specificReports = [];
+//         element.specificReports.forEach((report) => {
+//           if (report.display) {
+//             newElement.specificReports.push(report);
+//           }
+//         });
+//         if (newElement.specificReports.length > 0) {
+//           tempDisplay.push(newElement);
+//         }
+//       }
+//     });
+//     this.authReportDisplay = tempDisplay;
+//   }
+
+//   onSearchChange() {
+//     this.search();
+//   }
+
+//   search() {
+//     var tempDisplay: ReportCategory[] = [];
+//     this.authReportDisplay.forEach((element) => {
+//       if (
+//         element.title.toUpperCase().indexOf(this.searchTerm.toUpperCase()) !==
+//         -1
+//       ) {
+//         tempDisplay.push(element);
+//       } else {
+//         var newElement: ReportCategory = JSON.parse(JSON.stringify(element));
+//         newElement.specificReports = [];
+//         element.specificReports.forEach((report) => {
+//           if (
+//             report.name.toUpperCase().indexOf(this.searchTerm.toUpperCase()) !==
+//             -1
+//           ) {
+//             newElement.specificReports.push(report);
+//           }
+//         });
+//         if (newElement.specificReports.length > 0) {
+//           tempDisplay.push(newElement);
+//         }
+//       }
+//     });
+//     this.uiReports = tempDisplay;
+//   }
+
+//   toDynamicReport = (report: ReportInterface): void => {
+//     console.log('To dynamic report');
+//     const dialogConfig = new MatDialogConfig();
+//     dialogConfig.disableClose = false;
+//     dialogConfig.autoFocus = true;
+//     dialogConfig.width = '800px';
+//     dialogConfig.data = {
+//       report: report,
+//       reportTitle: report.name,
+//     };
+//     const dialogRef = this.dialog.open(
+//       DynamicReportLookupComponent,
+//       dialogConfig
+//     );
+//     dialogRef.afterClosed().subscribe((result) => {
+//       console.log(result);
+//       if (result != null && result.event == 'display') {
+//         this.displayReport = true;
+//         this.displayContents = result;
+//       }
+//     });
+//   };
+
+//   initDisplay = (): void => {
+//     console.log(this.retailcustomerreport);
+//     this.reportDisplay = [
+//       {
+//         title: 'Ticket Reports',
+//         display: true,
+//         specificReports: [
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'all_members.jrxml',
+//             name: 'Customer Report',
+//             display: this.retailcustomerreport,
+//           },
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'real_members.jrxml',
+//             name: 'Category Report',
+//             display: this.retailcustomerreport,
+//           },
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'member_by_category.jrxml',
+//             name: 'Ticket Assignee Report',
+//             display: this.retailcustomerreport,
+//           },
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'all_members.jrxml',
+//             name: 'All Ticket REPORT',
+//             display: this.retailcustomerreport,
+//           },
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'all_members.jrxml',
+//             name: 'Ticket Status REPORT',
+//             display: this.retailcustomerreport,
+//           },
+//           {
+//             isDynamic: true,
+//             requiredFields: ['fromdate', 'todate'],
+//             filename: 'all_members.jrxml',
+//             name: 'Institution Ticket REPORT',
+//             display: this.retailcustomerreport,
+//           },
+//         ],
+//       }, 
+//     ];
+//   };
+// }
